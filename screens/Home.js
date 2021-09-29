41b68feb6568fe42af4b390cedacbeb4908e619d
mohd owais <okhan27221@gmail.com>
2021-09-29 22:02:13 +0530

    done with categories.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text ,ScrollView} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Categories from '../components/Categories';
 import HeaderTabs from '../components/HeaderTabs';
+import RestaurantItem from '../components/RestaurantItem';
 import SearchBar from '../components/SearchBar';
 export default function Home() {
   return (
    
-    <SafeAreaView>
+    <SafeAreaView style={{backgroundColor:'#eee',flex:1}}>
+      <View style={{backgroundColor:'white',padding:25}}>
       <HeaderTabs/>
       <SearchBar/>
+      </View>
+      <ScrollView>
+      <Categories/>
+      <RestaurantItem/>
+      </ScrollView>
     </SafeAreaView>
     
   );
