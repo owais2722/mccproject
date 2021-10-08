55deaa08a97dce429f4abb666a1e0c44a9c8c989
mohd owais <okhan27221@gmail.com>
2021-10-08 19:49:27 +0530

    done till order confirmation

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import * as React from "react";
 import { View, Text } from "react-native";
 import Home from "../fooddel/screens/Home";
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import RootNavigation from "./navigation";
 export default function App() {
   return(
-  <SafeAreaProvider>
-  <Home/>
-  </SafeAreaProvider>); 
+ 
+ <RootNavigation />
+  ); 
 
    ;
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,15 @@
 import React ,{useState ,useEffect}from 'react'
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import { View, Text ,ScrollView} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context';
-import Categories from '../components/Categories';
-import HeaderTabs from '../components/HeaderTabs';
-import RestaurantItems ,{localRestaurants} from '../components/RestaurantItems';
+import Categories from '../components/home/Categories';
+import HeaderTabs from '../components/home/HeaderTabs';
+import RestaurantItems ,{localRestaurants} from '../components/home/RestaurantItems';
 import { Divider } from "react-native-elements";
-import NewSearchBar from '../components/NewSearchBar';
-import BottomTabs from '../components/BottomTabs';
+import NewSearchBar from '../components/home/NewSearchBar';
+import BottomTabs from '../components/home/BottomTabs';
 const YELP_API_KEY = "z7ntXGQN2VWvlfeg15WnfafVJUfbRBg4EHlDj0nJiGKe40bdpf-vXRpDjNctbAej6SppdE-zadxzyM7cbIvs8CPW_Z77UWN6HM82ubKvPwrt--4j_9un0-9FcGxVYXYx"
-export default function Home() {
+export default function Home({ navigation }) {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
   const [city, setCity] = useState("SanDiego");
   const [activeTab, setActiveTab] = useState("Delivery");
@@ -34,19 +35,19 @@ export default function Home() {
   },[city,activeTab])
   
   return (
-    <SafeAreaView style={{backgroundColor:'#eee',flex:1}}>
+    <SafeAreaProvider style={{backgroundColor:'#eee',flex:1}}>
       <View style={{backgroundColor:'white',padding:25}}>
       <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
       {/* <SearchBar/> */}
       <NewSearchBar cityHandler={setCity}/>
       </View>
-      <ScrollView >
+      <ScrollView showsVerticalScrollIndicator={false}>
       <Categories/>
-     <RestaurantItems restaurantData={restaurantData}/>
+     <RestaurantItems restaurantData={restaurantData}  navigation={navigation} />
       </ScrollView>
       <Divider width={1} />
       <BottomTabs />
-    </SafeAreaView>
+    </SafeAreaProvider>
     
   );
 }
