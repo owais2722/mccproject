1fb89765356ed32cca28f84265475e8a035339bb
khan mohd owais <60808895+owais2722@users.noreply.github.com>
2021-10-10 23:57:53 +0530

    removed firebase details

--- firebase.js
+++ firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDo8LmzwheyvLmKlm3XiRF1_Ba8uRcAtX0",
-    authDomain: "fooddel-999fb.firebaseapp.com",
-    projectId: "fooddel-999fb",
-    storageBucket: "fooddel-999fb.appspot.com",
-    messagingSenderId: "339803020493",
-    appId: "1:339803020493:web:f82501182edda80ebb9d84"
+   
   };
   
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
